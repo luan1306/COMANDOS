fd80955ea3dd5f8b61e6963b23393e1e8ed8e7c0
luan1306 <luanvtorres@ucundinamarca.edu.co>
2025-08-29 18:02:13 -0500

    cambios

--- src/Main.java
+++ src/Main.java
@@ -5,6 +5,15 @@ public class Main {
         //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
         // to see how IntelliJ IDEA suggests fixing it.
         System.out.printf("Comandos git");
+        System.out.printf("Comandos git");
+        System.out.printf("Comandos git");
+        System.out.printf("Comandos git");
+        System.out.printf("Comandos git");
+        System.out.printf("Comandos git");
+        System.out.printf("Comandos git");
+        System.out.printf("Comandos git");
+
+
 
 
     }
